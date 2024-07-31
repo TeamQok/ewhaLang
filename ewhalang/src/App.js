9286d54494a6f8064af2bfcce4108eb53eb4dfe9
hyeonsu <hskim2265@ewhain.net>
2024-07-31 20:16:59 +0900

    ✅ : 상단바 x버튼 추가

--- ewhalang/src/App.js
+++ ewhalang/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // 페이지 임포트
 import MainPage from "./pages/MainPage";
 import Login from "./pages/Login";
+import Onboarding from "./pages/Onboarding";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
 
           {/*로그인 페이지 */}
           <Route exact path="/login" element={<Login />} />
+          {/* 초기화면 페이지 */}
+          <Route exact path="/onboarding" element={<Onboarding />} />
         </Routes>
       </BrowserRouter>
     </>
