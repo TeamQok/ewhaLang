19e1391b0b355561eff1042f25d8891ceccacac0
hak2711 <hak271111@gmail.com>
2024-08-03 20:52:27 +0900

    🐛 : Modal background가 상단바, 하단바까지 포함하도록 수정

--- ewhalang/src/components/common/Modal.jsx
+++ ewhalang/src/components/common/Modal.jsx
@@ -8,8 +8,9 @@ const ModalBackground = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
+  z-index: 1001;
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
