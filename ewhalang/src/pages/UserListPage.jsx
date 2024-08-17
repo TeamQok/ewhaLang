88424f8408a54788c4014da5c1931763a7cb22b5
hyeonsu <hskim2265@ewhain.net>
2024-08-18 00:57:34 +0900

    충돌해결

--- ewhalang/src/pages/UserListPage.jsx
+++ ewhalang/src/pages/UserListPage.jsx
@@ -1,17 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import UserListInformation from '../components/pages/UserListInformation';
-import users from '../_mock/userMockData';
-import Topbar from '../components/layout/Topbar';
-import BottomBar from '../components/layout/BottomBar';
-import FilterComponent from '../components/pages/FilterComponent';
-import LanguageLevelInfo from '../components/pages/LanguageLevelInfo';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import UserListInformation from "../components/pages/UserListInformation";
+import users from "../_mock/userMockData";
+import Topbar from "../components/layout/Topbar";
+import BottomBar from "../components/layout/BottomBar";
+import FilterComponent from "../components/pages/FilterComponent";
+import LanguageLevelInfo from "../components/pages/LanguageLevelInfo";
 
+const UserListPage = () => {
+  const [filteredUsers, setFilteredUsers] = useState(users);
+  const [filterCriteria, setFilterCriteria] = useState({
+    languages: [],
+    countries: [],
+    gender: "전체",
+    birthdateRange: { start: 1996, end: 2005 },
+  });
 
   const loggedInUserId = "user1";
 
   useEffect(() => {
     const filtered = users.filter((user) => {
+      //로그인한 사용자 제외
+      if (user.userId === loggedInUserId) return false;
+
       const languageMatch =
         filterCriteria.languages.length === 0 ||
         filterCriteria.languages.some((lang) =>
@@ -43,59 +54,14 @@ import LanguageLevelInfo from '../components/pages/LanguageLevelInfo';
     }));
   };
 
-
-const UserListPage = () => {
-  const [filteredUsers, setFilteredUsers] = useState(users);
-  const [filterCriteria, setFilterCriteria] = useState({
-    languages: [],
-    countries: [],
-    gender: '전체',
-    birthdateRange: { start: 1996, end: 2005 }
-  });
-
-  const loggedInUserId = 'user1';
-
-  useEffect(() => {
-    const filtered = users.filter(user => {
-      //로그인한 사용자 제외
-      if(user.userId === loggedInUserId) return false;
-
-      const languageMatch = filterCriteria.languages.length === 0 || 
-        filterCriteria.languages.some(lang => 
-          user.languages.some(language => language.language === lang)
-        );
-      
-      const countryMatch = filterCriteria.countries.length === 0 || 
-        filterCriteria.countries.includes(user.country);
-
-      const genderMatch = filterCriteria.gender === '전체' || user.gender === filterCriteria.gender;
-  
-      const birthYearMatch = parseInt(user.birthdate) >= filterCriteria.birthdateRange.start && 
-        parseInt(user.birthdate) <= filterCriteria.birthdateRange.end;
-
-      return languageMatch && countryMatch && genderMatch && birthYearMatch;
-    });
-
-    setFilteredUsers(filtered);
-  }, [filterCriteria]);
-
-  const handleFilterChange = (newFilterCriteria) => {
-    setFilterCriteria(prevCriteria => ({
-      ...prevCriteria,
-      ...newFilterCriteria
-    }));
-  };
-
   return (
     <Wrapper>
-
-      <Topbar title={"이화랑 친구 찾기"}/>
-      <FilterComponent 
+      <Topbar title={"이화랑 친구 찾기"} />
+      <FilterComponent
         onFilterChange={handleFilterChange}
         initialFilterCriteria={filterCriteria}
       />
-      <LanguageLevelInfo/>
-
+      <LanguageLevelInfo />
 
       <ContentsWrapper>
         {filteredUsers.map((user, index) => (
